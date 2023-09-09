7636647130237549afc42b0345372dc73695666f
Clemens Horn <clemens.horn@mni.thm.de>
2023-09-09 20:17:15 +0200

    refactor(nx-cmake): lib generator

--- packages/nx-cmake/src/generators/library/generator.ts
+++ packages/nx-cmake/src/generators/library/generator.ts
@@ -1,5 +1,5 @@
 import { join } from 'path';
-import type { LibGeneratorSchema } from './schema';
+import type { GoogleTestInclude, LibGeneratorSchema } from './schema';
 import {
     type Tree,
     addProjectConfiguration,
@@ -8,9 +8,70 @@ import {
     offsetFromRoot,
     workspaceLayout,
 } from '@nx/devkit';
-import { CProjectType } from '../../models/types';
+import { type C, CProjectType } from '../../models/types';
 import { resolveOptions } from '../../utils/resolveOptions/resolveOptions';
 import { getProjectTargets } from '../../utils/getProjectTargets/getProjectTargets';
+
+export const getLibName = (name: string) => `lib${name}`;
+
+export const getTestName = (name: string) => `test${name}`;
+
+export const getTestLib = (language: C) => {
+    return language === 'C++' ? 'gtest' : 'cmocka';
+};
+
+export const getGoogleTestInclude = (
+    generateTests: boolean,
+    language: C
+): GoogleTestInclude => {
+    return generateTests && language === 'C++' ? 'include(GoogleTest)' : '';
+};
+
+export const getTestSetup = (
+    generateTests: boolean,
+    language: C,
+    testName: string
+) => {
+    return generateTests && language === 'C'
+        ? `add_test(UnitTests ${testName})`
+        : `gtest_discover_tests(${testName})`;
+};
+
+export const getBaseGoogleTest = (libName: string, projectName: string) => {
+    return (
+        `TEST(${libName}, test_${projectName}) {\n` +
+        `\tEXPECT_EQ(${projectName}(), 0);\n}\n`
+    );
+};
+
+export const getBaseCmockaTest = (projectName: string) => {
+    return (
+        `static int setup(void **state) {\n` +
+        `\t(void) state;\n` +
+        `\treturn 0;\n}\n\n` +
+        `static int teardown(void **state) {\n` +
+        `\t(void) state;\n` +
+        `\treturn 0;\n}\n\n` +
+        `static void test_${projectName}(void **state) {\n` +
+        `\t(void) state;\n` +
+        `\t${projectName}();\n}\n\n` +
+        `int main(void) {\n` +
+        `\tconst struct CMUnitTest ${projectName}_tests[] = {\n` +
+        `\t\tcmocka_unit_test(test_${projectName}),\n\t};\n` +
+        `\treturn cmocka_run_group_tests(${projectName}_tests, setup, teardown);\n}\n`
+    );
+};
+
+export const getBaseTest = (
+    generateTests: boolean,
+    language: C,
+    libName: string,
+    projectName: string
+) => {
+    const baseGtest = getBaseGoogleTest(libName, projectName);
+    const baseCmocka = getBaseCmockaTest(projectName);
+    return generateTests && language === 'C++' ? baseGtest : baseCmocka;
+};
 
 export async function libGenerator(tree: Tree, options: LibGeneratorSchema) {
     const resolvedOptions = resolveOptions(options);
@@ -20,33 +81,12 @@ export async function libGenerator(tree: Tree, options: LibGeneratorSchema) {
     const relativeRootPath = offsetFromRoot(projectRoot);
     const targets = getProjectTargets(CProjectType.Lib);
     const testTargets = getProjectTargets(CProjectType.Test);
-    const lib = `lib${name}`;
-    const test = `test${name}`;
-    const testLib = language === 'C++' ? 'gtest' : 'cmocka';
-    const includeGoogleTest =
-        generateTests && language === 'C++' ? 'include(GoogleTest)' : '';
-    const setupTests =
-        generateTests && language === 'C'
-            ? `add_test(UnitTests ${test})`
-            : `gtest_discover_tests(${test})`;
-    const baseGtest =
-        `TEST(${lib}, test_${name}) {\n` + `\tEXPECT_EQ(${name}(), 0);\n}\n`;
-    const baseCmocka =
-        `static int setup(void **state) {\n` +
-        `\t(void) state;\n` +
-        `\treturn 0;\n}\n\n` +
-        `static int teardown(void **state) {\n` +
-        `\t(void) state;\n` +
-        `\treturn 0;\n}\n\n` +
-        `static void test_${name}(void **state) {\n` +
-        `\t(void) state;\n` +
-        `\t${name}();\n}\n\n` +
-        `int main(void) {\n` +
-        `\tconst struct CMUnitTest ${name}_tests[] = {\n` +
-        `\t\tcmocka_unit_test(test_${name}),\n\t};\n` +
-        `\treturn cmocka_run_group_tests(${name}_tests, setup, teardown);\n}\n`;
-    const baseTest =
-        generateTests && language === 'C++' ? baseGtest : baseCmocka;
+    const libName = getLibName(name);
+    const testName = getTestName(name);
+    const testLib = getTestLib(language);
+    const includeGoogleTest = getGoogleTestInclude(generateTests, language);
+    const setupTests = getTestSetup(generateTests, language, testName);
+    const baseTest = getBaseTest(generateTests, language, libName, name);
 
     resolvedOptions.includeGoogleTest = includeGoogleTest;
     resolvedOptions.setupTests = setupTests;
@@ -54,7 +94,7 @@ export async function libGenerator(tree: Tree, options: LibGeneratorSchema) {
     resolvedOptions.testLib = testLib;
     resolvedOptions.relativeRootPath = relativeRootPath;
 
-    addProjectConfiguration(tree, lib, {
+    addProjectConfiguration(tree, libName, {
         root: projectRoot,
         projectType: 'library',
         sourceRoot: `${projectRoot}/src`,
@@ -69,7 +109,7 @@ export async function libGenerator(tree: Tree, options: LibGeneratorSchema) {
         resolvedOptions
     );
 
-    addProjectConfiguration(tree, test, {
+    addProjectConfiguration(tree, testName, {
         root: `${projectRoot}/test`,
         projectType: 'application',
         sourceRoot: `${projectRoot}/src/test`,
--- packages/nx-cmake/src/generators/library/schema.d.ts
+++ packages/nx-cmake/src/generators/library/schema.d.ts
@@ -1,9 +1,11 @@
 import type { BaseOptions } from '../../models/base';
 
+export type GoogleTestInclude = 'include(GoogleTest)' | '';
+
 export interface LibGeneratorSchema extends BaseOptions {
     testLib: 'gtest' | 'cmocka';
     generateTests: boolean = true;
     setupTests: string;
-    includeGoogleTest: 'include(GoogleTest)' | '';
+    includeGoogleTest: GoogleTestInclude;
     baseTest: string;
 }
