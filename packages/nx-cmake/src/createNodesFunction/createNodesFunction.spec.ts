56155c610786daf8a24445bb37dcd7e461a4c916
Clemens Horn <clemens.horn@mni.thm.de>
2023-09-10 23:56:21 +0200

    test(nx-cmake): createNodesFunction

--- packages/nx-cmake/src/createNodesFunction/createNodesFunction.spec.ts
+++ packages/nx-cmake/src/createNodesFunction/createNodesFunction.spec.ts
@@ -1,7 +1,122 @@
-import { createNodesFunction } from './createNodesFunction';
+import type { CreateNodesContext } from '@nx/devkit';
+import { createNodesFunction } from './createNodesFunction'; // Import your module
+import * as getProjectTypeModule from '../utils/generatorUtils/getProjectType/getProjectType';
+import * as getProjectConfiguration from './../utils/generatorUtils/getProjectConfiguration/getProjectConfiguration';
+import { CProjectType } from '../models/types';
 
 describe('createNodesFunction', () => {
-    test('should create nodes', () => {
-        expect(createNodesFunction).toBeDefined();
+    it('should return nodes based on the provided project configuration file', () => {
+        const mockGetProjectType = jest.spyOn(
+            getProjectTypeModule,
+            'getProjectType'
+        );
+
+        const mockGetProjectConfiguration = jest.spyOn(
+            getProjectConfiguration,
+            'getProjectConfiguration'
+        );
+
+        const getProjectConfigurationReturnMock: ReturnType<
+            typeof getProjectConfiguration.getProjectConfiguration
+        > = {
+            'nx-cmake-test': {
+                name: 'nx-cmake-test',
+                root: 'nx-cmake-test',
+                sourceRoot: 'nx-cmake-test/src',
+                projectType: 'application',
+                targets: {
+                    cmake: {
+                        dependsOn: ['^cmake'],
+                        inputs: ['cmake'],
+                        executor: 'nx-cmake:cmake',
+                        defaultConfiguration: 'development',
+                        configurations: {
+                            development: {
+                                release: false,
+                                args: [],
+                            },
+                            production: { release: true, args: [] },
+                        },
+                        options: {},
+                    },
+                    build: {
+                        dependsOn: ['^build', 'cmake'],
+                        inputs: ['production', '^production'],
+                        executor: 'nx-cmake:build',
+                        defaultConfiguration: 'development',
+                        configurations: {
+                            development: { args: [] },
+                            production: { args: [] },
+                        },
+                        options: {},
+                    },
+                    lint: {
+                        executor: 'nx-cmake:lint',
+                        defaultConfiguration: 'development',
+                        configurations: {
+                            development: { args: [] },
+                            production: { args: [] },
+                        },
+                        options: {},
+                    },
+                    fmt: {
+                        executor: 'nx-cmake:format',
+                        defaultConfiguration: 'development',
+                        configurations: {
+                            development: { args: [] },
+                            production: { args: [] },
+                        },
+                        options: {},
+                    },
+                    debug: {
+                        dependsOn: ['build'],
+                        inputs: ['default'],
+                        executor: 'nx-cmake:debug',
+                        defaultConfiguration: 'development',
+                        configurations: {
+                            development: { args: [] },
+                            production: { args: [] },
+                        },
+                        options: {},
+                    },
+                    execute: {
+                        dependsOn: ['build'],
+                        inputs: ['default'],
+                        executor: 'nx-cmake:execute',
+                        defaultConfiguration: 'development',
+                        configurations: {
+                            development: { args: [] },
+                            production: { args: [] },
+                        },
+                        options: {},
+                    },
+                },
+                tags: ['c'],
+                implicitDependencies: [],
+            },
+        };
+
+        const getProjectTypeReturnMock = CProjectType.App;
+
+        const root = 'packages';
+        const projectConfigurationFile = `${root}/CMakeLists.txt`;
+        const context: CreateNodesContext = {} as unknown as CreateNodesContext;
+
+        mockGetProjectType.mockReturnValue(getProjectTypeReturnMock);
+        mockGetProjectConfiguration.mockReturnValue(
+            getProjectConfigurationReturnMock
+        );
+
+        const result = createNodesFunction(projectConfigurationFile, context);
+
+        expect(result).toEqual({ projects: getProjectConfigurationReturnMock });
+        expect(mockGetProjectType).toHaveBeenCalledWith(root);
+        expect(mockGetProjectConfiguration).toHaveBeenCalledWith(
+            root,
+            getProjectTypeReturnMock
+        );
+
+        mockGetProjectType.mockRestore();
+        mockGetProjectConfiguration.mockRestore();
     });
 });
--- packages/nx-cmake/src/createNodesFunction/createNodesFunction.ts
+++ packages/nx-cmake/src/createNodesFunction/createNodesFunction.ts
@@ -1,11 +1,13 @@
-import type { CreateNodesFunction } from '@nx/devkit';
+import type { CreateNodesContext, CreateNodesFunction } from '@nx/devkit';
 import { projectFile } from '../config/projectFilePattern';
 import { getProjectType } from '../utils/generatorUtils/getProjectType/getProjectType';
 import { getProjectConfiguration } from '../utils/generatorUtils/getProjectConfiguration/getProjectConfiguration';
 
 export const createNodesFunction: CreateNodesFunction = (
-    projectConfigurationFile: string
+    projectConfigurationFile: string,
+    context: CreateNodesContext
 ) => {
+    context;
     const [root] = projectConfigurationFile.split(`/${projectFile}`);
     const type = getProjectType(root);
     const projects = getProjectConfiguration(root, type);
