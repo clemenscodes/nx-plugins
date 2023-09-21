ab616362951e156c28c096794acb6e145888f32c
Clemens Horn <clemens.horn@mni.thm.de>
2023-09-21 17:30:27 +0200

    test(nx-cmake): test build and cmake executors

--- packages/nx-cmake/src/executors/build/utils/buildProjectWithMake/buildProjectWithMake.spec.ts
+++ packages/nx-cmake/src/executors/build/utils/buildProjectWithMake/buildProjectWithMake.spec.ts
@@ -1,4 +1,5 @@
 import type { BuildExecutorSchema } from '../../schema';
+import { buildProjectWithMake } from './buildProjectWithMake';
 import * as runCommandModule from '../../../../utils/commandUtils/runCommand/runCommand';
 import * as checkCommandExistsModule from '../../../../utils/commandUtils/checkCommandExists/checkCommandExists';
 
@@ -22,8 +23,51 @@ describe('buildProjectWithMake', () => {
         jest.restoreAllMocks();
     });
 
-    it.todo('should build project with make and return true');
-    it.todo('should error if make is not installed');
-    it.todo('should pass arguments to make');
-    it.todo('should return false if make failed');
+    it('should call checkCommandExists with "make"', () => {
+        runCommandMock.mockReturnValue({ success: true });
+        buildProjectWithMake('workspaceRoot', 'projectRoot', options);
+        expect(checkCommandExistsMock).toHaveBeenCalledWith('make');
+    });
+
+    it('should error if make is not installed', () => {
+        checkCommandExistsMock.mockImplementation(() => {
+            throw new Error();
+        });
+        expect(() =>
+            buildProjectWithMake('workspaceRoot', 'projectRoot', options)
+        ).toThrowError();
+        expect(checkCommandExistsMock).toHaveBeenCalledWith('make');
+        expect(runCommandMock).not.toHaveBeenCalled();
+    });
+
+    it('should call runCommand with the correct arguments', () => {
+        runCommandMock.mockReturnValue({ success: true });
+        buildProjectWithMake('workspaceRoot', 'projectRoot', options);
+        expect(runCommandMock).toHaveBeenCalledWith(
+            'make',
+            '-C',
+            'workspaceRoot/dist/projectRoot',
+            ...options.args
+        );
+    });
+
+    it('should return true if runCommand succeeds', () => {
+        runCommandMock.mockReturnValue({ success: true });
+        const result = buildProjectWithMake(
+            'workspaceRoot',
+            'projectRoot',
+            options
+        );
+        expect(result).toBe(true);
+    });
+
+    it('should return false if runCommand fails', () => {
+        runCommandMock.mockReturnValue({ success: false });
+        const result = buildProjectWithMake(
+            'workspaceRoot',
+            'projectRoot',
+            options
+        );
+        expect(result).toBe(false);
+    });
 });
--- packages/nx-cmake/src/executors/cmake/utils/configureProjectWithCMake/configureProjectWithCMake.spec.ts
+++ packages/nx-cmake/src/executors/cmake/utils/configureProjectWithCMake/configureProjectWithCMake.spec.ts
@@ -1,6 +1,7 @@
 import type { CmakeExecutorSchema } from '../../schema';
 import * as runCommandModule from '../../../../utils/commandUtils/runCommand/runCommand';
 import * as checkCommandExistsModule from '../../../../utils/commandUtils/checkCommandExists/checkCommandExists';
+import { configureProjectWithCMake } from './configureProjectWithCMake';
 
 describe('buildProjectWithMake', () => {
     let runCommandMock: jest.SpyInstance;
@@ -24,8 +25,73 @@ describe('buildProjectWithMake', () => {
         jest.restoreAllMocks();
     });
 
-    it.todo('should configure project with cmake and return true');
-    it.todo('should error if cmake is not installed');
-    it.todo('should pass arguments to cmake');
-    it.todo('should return false if cmake failed');
+    it('should configure project with cmake and return true', () => {
+        runCommandMock.mockReturnValue({ success: true });
+        const result = configureProjectWithCMake(
+            'workspaceRoot',
+            'projectRoot',
+            options
+        );
+        expect(checkCommandExistsMock).toHaveBeenCalledWith('cmake');
+        expect(runCommandMock).toHaveBeenCalledWith(
+            'cmake',
+            '-S',
+            'workspaceRoot/projectRoot',
+            'workspaceRoot/dist/projectRoot',
+            '-DCMAKE_BUILD_TYPE=Debug',
+            ...options.args
+        );
+        expect(result).toBe(true);
+    });
+
+    it('should error if cmake is not installed', () => {
+        checkCommandExistsMock.mockImplementation(() => {
+            throw new Error();
+        });
+        expect(() =>
+            configureProjectWithCMake('workspaceRoot', 'projectRoot', options)
+        ).toThrowError();
+        expect(checkCommandExistsMock).toHaveBeenCalledWith('cmake');
+        expect(runCommandMock).not.toHaveBeenCalled();
+    });
+
+    it('should pass arguments to cmake', () => {
+        runCommandMock.mockReturnValue({ success: true });
+        options.args = ['--arg1', '--arg2'];
+        const result = configureProjectWithCMake(
+            'workspaceRoot',
+            'projectRoot',
+            options
+        );
+        expect(checkCommandExistsMock).toHaveBeenCalledWith('cmake');
+        expect(runCommandMock).toHaveBeenCalledWith(
+            'cmake',
+            '-S',
+            'workspaceRoot/projectRoot',
+            'workspaceRoot/dist/projectRoot',
+            '-DCMAKE_BUILD_TYPE=Debug',
+            '--arg1',
+            '--arg2'
+        );
+        expect(result).toBe(true);
+    });
+
+    it('should return false if cmake failed', () => {
+        runCommandMock.mockReturnValue({ success: false });
+        const result = configureProjectWithCMake(
+            'workspaceRoot',
+            'projectRoot',
+            options
+        );
+        expect(checkCommandExistsMock).toHaveBeenCalledWith('cmake');
+        expect(runCommandMock).toHaveBeenCalledWith(
+            'cmake',
+            '-S',
+            'workspaceRoot/projectRoot',
+            'workspaceRoot/dist/projectRoot',
+            '-DCMAKE_BUILD_TYPE=Debug',
+            ...options.args
+        );
+        expect(result).toBe(false);
+    });
 });
