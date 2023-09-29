65e80334a0482bb5f8283d636b6caa0b5c35f535
Clemens Horn <clemens.horn@mni.thm.de>
2023-09-29 18:44:30 +0200

    fix(nx-cmake): deactivate husky to allow commits

--- packages/nx-cmake/src/utils/commandUtils/commandExists/commandExists.ts
+++ packages/nx-cmake/src/utils/commandUtils/commandExists/commandExists.ts
@@ -1,10 +1,22 @@
 import { executeCommand } from '../executeCommand/executeCommand';
 
+export const checkCommandExistsWindows = (command: string): boolean => {
+    const cmd = `where ${command}`;
+    return !!executeCommand(cmd);
+};
+
+export const checkCommandExistsUnix = (command: string): boolean => {
+    const cmd = `command -v ${command}`;
+    return !!executeCommand(cmd);
+};
+
 export const commandExists = (command: string): boolean => {
     try {
         const [beforeSpace] = command.split(' ');
-        const cmd = `command -v ${beforeSpace}`;
-        return !!executeCommand(cmd);
+        if (process.platform === 'win32') {
+            return checkCommandExistsWindows(beforeSpace);
+        }
+        return checkCommandExistsUnix(beforeSpace);
     } catch {
         return false;
     }
