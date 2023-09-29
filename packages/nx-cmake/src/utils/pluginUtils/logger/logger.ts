ecb115672ff6528f5297e267f042cb19fc9137fa
Clemens Horn <clemens.horn@mni.thm.de>
2023-09-29 15:57:29 +0200

    fix(nx-cmake): capture signals, add verbose logging

---
+++ packages/nx-cmake/src/utils/pluginUtils/logger/logger.ts
@@ -0,0 +1,7 @@
+import { output } from '@nx/devkit';
+
+export const logger = (title: string, ...messages: string[]) => {
+    if (process.env.NX_VERBOSE_LOGGING === 'true') {
+        output.log({ title, bodyLines: messages });
+    }
+};
