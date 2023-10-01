356255eceeea2e8f7352aeaa26f65fb641c13c7a
Clemens Horn <clemens.horn@mni.thm.de>
2023-10-01 21:24:49 +0200

    ci(nx-cmake): tst

--- packages/nx-cmake/src/generators/init/utils/generateRootConfig/generateRootConfig.spec.ts
+++ packages/nx-cmake/src/generators/init/utils/generateRootConfig/generateRootConfig.spec.ts
@@ -58,11 +58,11 @@ describe('generateRootConfig', () => {
             `set(CMAKE_LIBRARY_PATH \${WORKSPACE_DIR}/dist/${options.libsDir})\n` +
             'set_global_settings()\n' +
             'if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")\n' +
-            '    set(CMAKE_C_COMPILER /usr/local/bin/gcc-13)\n' +
-            '    set(CMAKE_CXX_COMPILER ${CMAKE_C_COMPILER})\n' +
+            '    set(CMAKE_C_COMPILER gcc-13 CACHE STRING "" FORCE)\n' +
+            '    set(CMAKE_CXX_COMPILER ${CMAKE_C_COMPILER} CACHE STRING "" FORCE)\n' +
             'else()\n' +
-            '    set(CMAKE_C_COMPILER gcc)\n' +
-            '    set(CMAKE_CXX_COMPILER ${CMAKE_C_COMPILER})\n' +
+            '    set(CMAKE_C_COMPILER gcc CACHE STRING "" FORCE)\n' +
+            '    set(CMAKE_CXX_COMPILER ${CMAKE_C_COMPILER} CACHE STRING "" FORCE)\n' +
             'endif()\n';
         expect(readRootConfig).toBe(normalizeLineEndings(expectedRootConfig));
     });
