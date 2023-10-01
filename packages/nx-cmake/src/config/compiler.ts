973e4c81c51c150f1ada6bc2b6823634ff2be7c7
Clemens Horn <clemens.horn@mni.thm.de>
2023-10-02 00:13:46 +0200

    ci(nx-cmake): test

--- packages/nx-cmake/src/config/compiler.ts
+++ packages/nx-cmake/src/config/compiler.ts
@@ -1,4 +1,4 @@
 export const DARWIN_GCC = '/usr/local/bin/gcc-13';
 // export const WINDOWS_GCC = 'C:/MinGW/mingw64/bin/gcc.exe';
-export const WINDOWS_GCC = 'gcc'
+export const WINDOWS_GCC = 'gcc';
 export const LINUX_GCC = 'gcc';
