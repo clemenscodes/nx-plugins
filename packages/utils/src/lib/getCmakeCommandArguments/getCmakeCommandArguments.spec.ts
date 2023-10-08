2e54cf7ed5d75b5210488e635f0800ba97fc0c9c
Clemens Horn <clemens.horn@mni.thm.de>
2023-10-08 17:16:31 +0200

    fix(nx-cmake): build tools passed to cmake command have cmake friendly path

--- packages/utils/src/lib/getCmakeCommandArguments/getCmakeCommandArguments.spec.ts
+++ packages/utils/src/lib/getCmakeCommandArguments/getCmakeCommandArguments.spec.ts
@@ -1,13 +1,22 @@
 import { getCmakeCommandArguments } from './getCmakeCommandArguments';
-import { CmakeExecutorSchema, LINUX_GCC } from '@/config';
+import {
+    CmakeExecutorSchema,
+    LINUX_GCC,
+    WINDOWS_GCC,
+    WINDOWS_MAKE,
+} from '@/config';
 import * as isWindowsModule from '../isWindows/isWindows';
 import * as getGccModule from '../getGcc/getGcc';
+import * as getMakeModule from '../getMake/getMake';
+import { join } from 'path';
 
 describe('getCmakeCommandArguments', () => {
     let workspaceRoot: string;
     let projectRoot: string;
     let options: CmakeExecutorSchema;
     let isWindowsMock: jest.SpyInstance;
+    let getMakeMock: jest.SpyInstance;
+    let getGccMock: jest.SpyInstance;
     let getGccReturnMock: string;
 
     beforeEach(() => {
@@ -18,21 +27,38 @@ describe('getCmakeCommandArguments', () => {
             args: [],
             release: false,
         };
-        isWindowsMock = jest.spyOn(isWindowsModule, 'isWindows');
-        jest.spyOn(getGccModule, 'getGcc').mockReturnValue(getGccReturnMock);
+        isWindowsMock = jest
+            .spyOn(isWindowsModule, 'isWindows')
+            .mockReturnValue(false);
+        getGccMock = jest
+            .spyOn(getGccModule, 'getGcc')
+            .mockReturnValue(getGccReturnMock);
+        getMakeMock = jest.spyOn(getMakeModule, 'getMake');
     });
 
     afterEach(() => {
         jest.restoreAllMocks();
     });
 
-    it('should get cmake command arguments', () => {
-        isWindowsMock.mockReturnValueOnce(false);
+    it('should get cmake command arguments on windows', () => {
+        isWindowsMock.mockReturnValue(true);
+        getMakeMock.mockReturnValue(WINDOWS_MAKE[0]);
+        getGccMock.mockReturnValue(WINDOWS_GCC[0]);
         const cmakeArguments = getCmakeCommandArguments(
             workspaceRoot,
             projectRoot,
             options,
         );
-        expect(cmakeArguments).toBeDefined();
+        const expectedCmakeArguments = [
+            '-S',
+            join('workspaceRoot/projectRoot'),
+            join('workspaceRoot/dist/projectRoot'),
+            '-G "MinGW Makefiles"',
+            '-DCMAKE_MAKE_PROGRAM=C:/msys64/ucrt64/bin/mingw32-make.exe',
+            '-DCMAKE_C_COMPILER=C:/msys64/ucrt64/bin/gcc.exe',
+            '-DCMAKE_CXX_COMPILER=C:/msys64/ucrt64/bin/gcc.exe',
+            '-DCMAKE_BUILD_TYPE=Debug',
+        ];
+        expect(cmakeArguments).toStrictEqual(expectedCmakeArguments);
     });
 });
--- packages/utils/src/lib/getCmakeCommandArguments/getCmakeCommandArguments.ts
+++ packages/utils/src/lib/getCmakeCommandArguments/getCmakeCommandArguments.ts
@@ -1,6 +1,8 @@
-import { CmakeExecutorSchema, WINDOWS_MAKE } from '@/config';
+import { CmakeExecutorSchema } from '@/config';
 import { getGcc } from '../getGcc/getGcc';
 import { isWindows } from '../isWindows/isWindows';
+import { getMake } from '../getMake/getMake';
+import { join } from 'path';
 
 export const getCmakeCommandArguments = (
     workspaceRoot: string,
@@ -9,18 +11,21 @@ export const getCmakeCommandArguments = (
 ): string[] => {
     const { release, args } = options;
     const gcc = getGcc();
+    const make = getMake();
+    const transformedGcc = gcc.replace(/\\/g, '/');
+    const transformedMake = make.replace(/\\/g, '/');
     const cmakeArguments = [
         '-S',
-        `${workspaceRoot}/${projectRoot}`,
-        `${workspaceRoot}/dist/${projectRoot}`,
+        join(workspaceRoot, projectRoot),
+        join(workspaceRoot, 'dist', projectRoot),
         ...(isWindows(process.platform)
             ? ['-G "MinGW Makefiles"']
             : ['-G "Unix Makefiles"']),
         ...(isWindows(process.platform)
-            ? [`-DCMAKE_MAKE_PROGRAM=${WINDOWS_MAKE}`]
+            ? [`-DCMAKE_MAKE_PROGRAM=${transformedMake}`]
             : []),
-        `-DCMAKE_C_COMPILER=${gcc}`,
-        `-DCMAKE_CXX_COMPILER=${gcc}`,
+        `-DCMAKE_C_COMPILER=${transformedGcc}`,
+        `-DCMAKE_CXX_COMPILER=${transformedGcc}`,
         `-DCMAKE_BUILD_TYPE=${release ? 'Release' : 'Debug'}`,
         ...args,
     ];
