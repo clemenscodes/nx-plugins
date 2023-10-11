61c8ff6c6e6216da48407ced617a23dec3d54380
Clemens Horn <clemens.horn@mni.thm.de>
2023-10-11 19:30:59 +0200

    test(file, graph): file 100% coverage, start graph coverage

--- packages/graph/src/lib/filterDependenciesOfProject/getGccDependencies/getGccDependencies.spec.ts
+++ packages/graph/src/lib/filterDependenciesOfProject/getGccDependencies/getGccDependencies.spec.ts
@@ -7,8 +7,15 @@ describe('getGccDependencies', () => {
     let executeCommandSpy: jest.SpyInstance;
     let detectTestFrameworkSpy: jest.SpyInstance;
     let installTestFrameworkSpy: jest.SpyInstance;
+    let loggerMock: jest.SpyInstance;
+    let cmd: string;
+    let projectRoot: string;
+    let workspaceRoot: string;
 
     beforeEach(() => {
+        cmd = 'gcc-command';
+        projectRoot = 'project-root';
+        workspaceRoot = '/workspace/root';
         executeCommandSpy = jest.spyOn(utilsModule, 'executeCommand');
         detectTestFrameworkSpy = jest.spyOn(
             detectTestFrameworkModule,
@@ -18,6 +25,7 @@ describe('getGccDependencies', () => {
             installTestFrameworkModule,
             'installTestFramework',
         );
+        loggerMock = jest.spyOn(utilsModule, 'logger');
     });
 
     afterEach(() => {
@@ -25,21 +33,29 @@ describe('getGccDependencies', () => {
     });
 
     it('should return GCC dependencies if no test framework detected', () => {
-        const cmd = 'gcc-command';
-        const projectRoot = 'project-root';
-        const workspaceRoot = '/workspace/root';
         executeCommandSpy.mockReturnValue('Some output');
-        detectTestFrameworkSpy.mockReturnValue(false);
         const result = getGccDependencies(cmd, projectRoot, workspaceRoot);
         expect(result).toBe('Some output');
         expect(executeCommandSpy).toHaveBeenCalledWith(cmd);
         expect(installTestFrameworkSpy).not.toHaveBeenCalled();
     });
 
+    it('should rethrow the error and log it if no test framework is detected', () => {
+        const mockErrorMessage = 'Some error';
+        const mockError = new Error(mockErrorMessage);
+        executeCommandSpy.mockImplementation(() => {
+            throw mockError;
+        });
+        detectTestFrameworkSpy.mockReturnValue(false);
+        expect(() =>
+            getGccDependencies(cmd, projectRoot, workspaceRoot),
+        ).toThrowError(mockErrorMessage);
+        expect(executeCommandSpy).toHaveBeenCalledWith(cmd);
+        expect(loggerMock).toHaveBeenCalledWith(JSON.stringify(mockError));
+        expect(installTestFrameworkSpy).not.toHaveBeenCalled();
+    });
+
     it('should install test framework and return stdout if test framework detected', () => {
-        const cmd = 'gcc-command';
-        const projectRoot = 'project-root';
-        const workspaceRoot = '/workspace/root';
         executeCommandSpy.mockImplementation(() => {
             throw new Error('Some error');
         });
@@ -54,5 +70,22 @@ describe('getGccDependencies', () => {
             projectRoot,
             cmd,
         );
+    });
+
+    it('should throw the error if it is not an instance of Error', () => {
+        const nonError = { someProperty: 'someValue' };
+        executeCommandSpy.mockImplementation(() => {
+            throw nonError;
+        });
+        let thrownError: unknown;
+        try {
+            getGccDependencies(cmd, projectRoot, workspaceRoot);
+        } catch (e) {
+            thrownError = e;
+        }
+        expect(thrownError).toBe(nonError);
+        expect(executeCommandSpy).toHaveBeenCalledWith(cmd);
+        expect(detectTestFrameworkSpy).not.toHaveBeenCalled();
+        expect(installTestFrameworkSpy).not.toHaveBeenCalled();
     });
 });
