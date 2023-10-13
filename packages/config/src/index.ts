92946398806aa8b39406b1d9cfd711e5aaeb4ddf
Clemens Horn <clemens.horn@mni.thm.de>
2023-10-13 10:11:34 +0200

    test(config): some test coverage hopefully

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,3 +1,13 @@
+export {
+    getGcc,
+    getGdb,
+    getCmake,
+    getClangTidy,
+    getClangFormat,
+    getMake,
+    getCtest,
+} from './lib/nx-cmake';
+
 export * from './lib/nx-cmake';
 export * from './lib/nx';
 
