82872fb0fa4421b6d16cf15fae7ef448278fa37f
Clemens Horn <clemens.horn@mni.thm.de>
2023-10-13 10:00:48 +0200

    chore(graph): refactor some util

--- packages/graph/src/lib/filterProjects/projectConfigContainsCTag/projectConfigContainsCTag.ts
+++ packages/graph/src/lib/filterProjects/projectConfigContainsCTag/projectConfigContainsCTag.ts
@@ -1,10 +1,12 @@
 import type { ProjectConfiguration } from '@nx/devkit';
 import { isC } from '../isC/isC';
 
-export const projectConfigContainsCTag = (config: ProjectConfiguration) => {
+export const projectConfigContainsCTag = (
+    config: ProjectConfiguration,
+): boolean => {
     const { tags } = config;
     if (!tags) {
-        throw new Error(`project configuration does not contain tags`);
+        return false;
     }
     return tags.some(isC);
 };
