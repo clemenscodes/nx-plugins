43b7b35170ed3abfe81a20e700692903a86267cc
Clemens Horn <clemens.horn@mni.thm.de>
2023-10-25 15:29:53 +0200

    ci(): test e2e
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- .verdaccio/src/lib/e2e.ts
+++ .verdaccio/src/lib/e2e.ts
@@ -5,16 +5,21 @@ import { exit } from 'node:process';
 
 export const e2e = (procedure: 'affected' | 'run-many') => {
     const registryProcess = startLocalRegistry();
-    execSync(
-        `nx ${procedure} -t e2e --output-style=stream --configuration=ci`,
-        {
-            stdio: 'inherit',
-            env: {
-                ...process.env,
-                SKIP: 'true',
-            },
+    const parallel = `--parallel=${
+        process.env.NX_CLOUD_DISTRIBUTED_EXECUTION_AGENT_COUNT
+            ? process.env.NX_CLOUD_DISTRIBUTED_EXECUTION_AGENT_COUNT
+            : 1
+    }`;
+    const head = process.env.NX_HEAD ? `--head=${process.env.NX_HEAD}` : '';
+    const base = process.env.NX_BASE ? `--base=${process.env.NX_BASE}` : '';
+    const cmd = `nx ${procedure} -t e2e --nx-bail --output-style=stream --configuration=ci ${base} ${head} ${parallel}`;
+    execSync(cmd, {
+        stdio: 'inherit',
+        env: {
+            ...process.env,
+            SKIP: 'true',
         },
-    );
+    });
     stopLocalRegistry(registryProcess);
     exit(0);
 };
