5f78dc941c5eb48b053c6e2c8cd14b90211ccf90
Clemens Horn <clemens.horn@mni.thm.de>
2023-10-25 14:00:10 +0200

    ci(): test e2e
    Signed-off-by: Clemens Horn <clemens.horn@mni.thm.de>

--- .verdaccio/src/lib/publishPackages.ts
+++ .verdaccio/src/lib/publishPackages.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'node:child_process';
 
 export const publishPackages = () => {
-    execSync('pnpm nx run-many --targets publish --ver 1.0.0 --tag e2e', {
+    execSync('pnpm nx affected --targets publish', {
         env: {
             ...process.env,
         },
