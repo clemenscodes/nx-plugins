793755ad016b8798596a95b867a14b85b8dcfdc0
Clemens Horn <clemens.horn@mni.thm.de>
2024-02-28 11:09:36 +0100

    build(nx-cmake): setup nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    utils,
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            corepack
+            cmake
+            gnumake
+            gdb
+            clang-tools
+          ];
+        };
+      }
+    );
+}
