d7e84de95fca81aff0161f67dece4bf73cd439da
Clemens Horn <clemens.horn@mni.thm.de>
2024-03-19 17:43:10 +0100

    chore(create-nx-cmake): default to main

--- plugins/create-nx-cmake/bin/index.ts
+++ plugins/create-nx-cmake/bin/index.ts
@@ -3,17 +3,18 @@
 import { createWorkspace } from 'create-nx-workspace';
 
 async function main() {
-    const name = process.argv[2]; // TODO: use libraries like yargs or enquirer to set your workspace name
+    const name = process.argv[2];
+
     if (!name) {
         throw new Error('Please provide a name for the workspace');
     }
 
     console.log(`Creating the workspace: ${name}`);
 
-    // TODO: update below to customize the workspace
     const { directory } = await createWorkspace(`nx-cmake`, {
         name,
         nxCloud: 'skip',
+        defaultBase: 'main',
         interactive: false,
         packageManager: 'npm',
     });
